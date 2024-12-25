5af01800eb0f8671ba171262aaa0728504a9ece6
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2024-12-25 18:47:29 +0300

    update1

--- 3821B1FI1/9_gelu_ocl/bonyuk_peter/gelu_ocl.cpp
+++ 3821B1FI1/9_gelu_ocl/bonyuk_peter/gelu_ocl.cpp
@@ -1,4 +1,4 @@
-﻿#include "gelu_ocl.h"
+#include "gelu_ocl.h"
 
 #include <CL/opencl.hpp>
 #include <iostream>
@@ -6,10 +6,10 @@
 #include <utility>
 
 std::vector<float> GeluOCL(const std::vector<float>& input) {
-	std::vector<cl::Platform> platform;
-	cl::Platform::get(&platform);
+	std::vector<cl::Platform> platforms;
+	cl::Platform::get(&platforms);
 
-	cl::Platform platform = platform.front();
+	cl::Platform platform = platforms.front();
 
 	std::vector<cl::Device> devices;
 	platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
